6e91161e645dafe61a3d63afa8ddac0358adad7f
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-20 22:45:40 -0600

    Cabecera

---
+++ Pila/pila.h
@@ -0,0 +1,15 @@
+#ifndef _PILA_H_
+#define _PILA_H_
+
+#include<stdio.h>
+#include <stdlib.h>
+struct Stack
+{
+  int data;
+  struct Stack *next;
+};
+
+struct Stack *Push (struct Stack *, int);
+struct Stack *Pop (struct Stack *);
+
+#endif
