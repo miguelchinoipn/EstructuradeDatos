497a3c881ecb69d216dcf0d4f06309ea548aa23d
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-20 19:36:43 -0600

    Listas Simplemente Ligadas
    Este es un programa el cual realiza 9 funciones diferentes con listas doblemente circulares, espero este bien el programa, de lo contrario, hacérmelo saber.
    Gracias.

--- lsSEnlazadas.c
+++ lsSEnlazadas.c
@@ -1,44 +1,37 @@
 /* Listas Simplemente Ligadas */
 #include<stdio.h>
 #include <stdlib.h>
-struct Lista
+struct ListaSLigada
 {
 
   int dato;
 
-  struct Lista *siguiente;
+  struct ListaSLigada *siguiente;
 
-  struct Lista *anterior;
+  struct ListaSLigada *anterior;
 
 };
 
 struct Lista *agregarElemento (struct Lista *, int);
-struct Lista *mezclarListas (struct Lista *, struct Lista *);
-void mostrarLista (struct Lista *);
-void mostrarInversa (struct Lista *);
-struct Lista *eliminarCola (struct Lista *);
-struct Lista *eliminarCabeza (struct Lista *);
-struct Lista *eliminarLista (struct Lista *);
-void buscarElemento (struct Lista *, int);
-struct Lista *buscarEliminar (struct Lista *, int);
+struct ListaSLigada *agregarElemento (struct ListaSLigada *, int);
+struct ListaSLigada *mezclarListas (struct ListaSLigada *, struct ListaSLigada *);
+void mostrarLista (struct ListaSLigada *);
+void mostrarInversa (struct ListaSLigada *);
+struct ListaSLigada *eliminarCola (struct ListaSLigada *);
+struct ListaSLigada *eliminarCabeza (struct ListaSLigada *);
+struct ListaSLigada *eliminarLista (struct ListaSLigada *);
+int buscarElemento (struct ListaSLigada *, int);
+struct ListaSLigada *buscarEliminar (struct ListaSLigada *, int);
 
 int
 main (void)
 {
 
+  struct ListaSLigada *lista1 = NULL;
+  struct ListaSLigada *lista2 = NULL;
+  struct ListaSLigada *lista3 = NULL;
 
-  struct Lista *lista1 = NULL;
-
-
-  struct Lista *lista2 = NULL;
-
-
-  struct Lista *lista3 = NULL;
-
-
-
-  int max, numero;
-
+  int max, numero,busqueda = 0;
 
   //Ingresa el numero total de nodos en la lista
   scanf ("%d", &max);
@@ -112,19 +105,17 @@ main (void)
 
   mostrarLista (lista3);
 
-
   printf ("\n\n");
-
 
   //Buscamos un Elemento ingresado en la lista3 
   scanf ("%d", &numero);
 
-  buscarElemento (lista3, numero);
+  busqueda = buscarElemento (lista3, numero);
+  printf("Si es 1 se encontro, si es 0, no se pudo encontrar: %d\n\n",busqueda);
 
   mostrarLista (lista3);
 
   printf ("\n\n");
-
 
   //Buscamos un Elemento y lo ELIMINAMOS de la lista3 
   scanf ("%d", &numero);
@@ -135,8 +126,6 @@ main (void)
 
   printf ("\n\n");
 
-
-
   //Eliminamos toda la lista
   lista3 = eliminarLista (lista3);
 
@@ -144,35 +133,34 @@ main (void)
 
   mostrarLista (lista3);
 
-
   printf ("\n");
 
   return 0;
 }
 
 
-struct Lista *
-agregarElemento (struct Lista *lista, int dato)
+struct ListaSLigada *
+agregarElemento (struct ListaSLigada *lista, int dato)
 {
   //Agrega elementos al inicio de la lista
-  struct Lista *nuevo = NULL;
-  nuevo = (struct Lista *) malloc (sizeof (struct Lista));
+  struct ListaSLigada *nuevo = NULL;
+  nuevo = (struct ListaSLigada *) malloc (sizeof (struct ListaSLigada));
   nuevo->dato = dato;
   nuevo->siguiente = lista;
 
   return nuevo;
 }
 
-/*struct Lista *
-agregarElemento (struct Lista *lista, int dato)
+/*struct ListaSLigada *
+agregarElemento (struct ListaSLigada *lista, int dato)
 {
 
   //Agrega elementos al final de la lista
-  struct Lista *aux;
+  struct ListaSLigada *aux;
 
-  struct Lista *nuevo = NULL;
+  struct ListaSLigada *nuevo = NULL;
 
-  nuevo = (struct Lista *) malloc (sizeof (struct Lista));
+  nuevo = (struct ListaSLigada *) malloc (sizeof (struct ListaSLigada));
 
   nuevo->dato = dato;
 
@@ -206,7 +194,7 @@ agregarElemento (struct Lista *lista, int dato)
 
 
 void
-mostrarLista (struct Lista *lista)
+mostrarLista (struct ListaSLigada *lista)
 {
 
   while (lista != NULL)
@@ -219,11 +207,11 @@ mostrarLista (struct Lista *lista)
     }
 }
 
-struct Lista *
-mezclarListas (struct Lista *lista1, struct Lista *lista2)
+struct ListaSLigada *
+mezclarListas (struct ListaSLigada *lista1, struct ListaSLigada *lista2)
 {
 
-  struct Lista *ListaMezclada = NULL;
+  struct ListaSLigada *ListaMezclada = NULL;
 
 
   while (lista1 != NULL || lista2 != NULL)
@@ -253,7 +241,7 @@ mezclarListas (struct Lista *lista1, struct Lista *lista2)
 }
 
 void
-mostrarInversa (struct Lista *lista)
+mostrarInversa (struct ListaSLigada *lista)
 {
   if (lista)
     {
@@ -262,11 +250,11 @@ mostrarInversa (struct Lista *lista)
     }
 }
 
-struct Lista *
-eliminarCola (struct Lista *lista)
+struct ListaSLigada *
+eliminarCola (struct ListaSLigada *lista)
 {
-  struct Lista *aux = NULL;
-  struct Lista *aux2 = NULL;
+  struct ListaSLigada *aux = NULL;
+  struct ListaSLigada *aux2 = NULL;
   aux2 = lista;
   aux = lista;
   while (aux->siguiente != NULL)
@@ -282,20 +270,20 @@ eliminarCola (struct Lista *lista)
   return lista;
 }
 
-struct Lista *
-eliminarCabeza (struct Lista *lista)
+struct ListaSLigada *
+eliminarCabeza (struct ListaSLigada *lista)
 {
-  struct Lista *aux = NULL;
+  struct ListaSLigada *aux = NULL;
   aux = lista;
   lista = lista->siguiente;
   free (aux);
   return lista;
 }
 
-struct Lista *
-eliminarLista (struct Lista *lista)
+struct ListaSLigada *
+eliminarLista (struct ListaSLigada *lista)
 {
-  struct Lista *aux = NULL;
+  struct ListaSLigada *aux = NULL;
   aux = lista;
   while (lista != NULL)
     {
@@ -305,34 +293,36 @@ eliminarLista (struct Lista *lista)
   return lista;
 }
 
-void
-buscarElemento (struct Lista *lista, int dato)
+int
+buscarElemento (struct ListaSLigada *lista, int dato)
 {
-  struct Lista *aux = NULL;
+  struct ListaSLigada *aux = NULL;
   int band = 0;
   aux = lista;
   while (aux != NULL)
     {
       if (aux->dato == dato)
 	{
-	  printf (" %d :)! \n", aux->dato);
-	  band = 1;
-	  break;
+	  //printf (" %d :)! \n", aux->dato);
+    band = 1;
+	  return 1;
+    break;
 	}
       aux = aux->siguiente;
     }
   if (band == 0)
     {
-      printf ("¡%d :(!! \n", dato);
+      //printf ("¡%d :(!! \n", dato);
+      return 0;
     }
 
 }
 
-struct Lista *
-buscarEliminar (struct Lista *lista, int dato)
+struct ListaSLigada *
+buscarEliminar (struct ListaSLigada *lista, int dato)
 {
-  struct Lista *aux = NULL;
-  struct Lista *aux2 = NULL;
+  struct ListaSLigada *aux = NULL;
+  struct ListaSLigada *aux2 = NULL;
   int band = 0;
   aux = lista;
   aux2 = lista;
