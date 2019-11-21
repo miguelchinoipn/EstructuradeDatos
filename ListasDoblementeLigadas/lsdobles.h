5d2632f2e0909b29642ab3cf0abe45036702a8bb
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-20 21:42:28 -0600

    Cabecera

--- ListasDoblementeLigadas/lsdobles.h
+++ ListasDoblementeLigadas/lsdobles.h
@@ -13,7 +13,7 @@ struct ListaDoble
 
 struct ListaDoble *agregarElemento (struct ListaDoble *, int);
 struct ListaDoble *mezclarListaDobles (struct ListaDoble *, struct ListaDoble *);
-void mostrarListaDoble (struct ListaDoble *);
+void mostrarLista (struct ListaDoble *);
 void mostrarInversa (struct ListaDoble *);
 struct ListaDoble *eliminarCola (struct ListaDoble *);
 struct ListaDoble *eliminarCabeza (struct ListaDoble *);
