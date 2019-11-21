fe42a8397a292835b7d9974cd25a10f231aebd5f
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-20 20:38:59 -0600

    Cabecera Listas Simplemente Ligadas
    Este archivo contiene los prototipos de las funciones que realiza el programa principal, así como las bibliotecas necesarias para su funcionamiento

--- ListasSimplementeLigadas/main.c
+++ ListasSimplementeLigadas/main.c
@@ -1,27 +1,3 @@
-/* Listas Simplemente Ligadas */
-#include<stdio.h>
-#include <stdlib.h>
-struct ListaSLigada
-{
-
-  int dato;
-
-  struct ListaSLigada *siguiente;
-
-  struct ListaSLigada *anterior;
-
-};
-
-struct ListaSLigada *agregarElemento (struct ListaSLigada *, int);
-struct ListaSLigada *mezclarListas (struct ListaSLigada *, struct ListaSLigada *);
-void mostrarLista (struct ListaSLigada *);
-void mostrarInversa (struct ListaSLigada *);
-struct ListaSLigada *eliminarCola (struct ListaSLigada *);
-struct ListaSLigada *eliminarCabeza (struct ListaSLigada *);
-struct ListaSLigada *eliminarLista (struct ListaSLigada *);
-int buscarElemento (struct ListaSLigada *, int);
-struct ListaSLigada *buscarEliminar (struct ListaSLigada *, int);
-
 int
 main (void)
 {
