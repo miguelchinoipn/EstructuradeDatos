4c25fc5858ca368d70108dd1ce792dfe751a5db4
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-20 19:47:46 -0600

    Cabezera Contiguas
    Este codigo incluye los prototipos de las funciones y las bibleoteas a utilizar en el programa

---
+++ ListasContiguas/contiguas.h
@@ -0,0 +1,19 @@
+#ifndef __CONTIGUAS_H__
+#define __CONTIGUAS_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+struct Contigua
+{
+  int *datos; //Guarda el ni
+  int actual; //El numero de la lista en el que se encuentra
+  int cuantos; //Cantidad de elementos de la lista
+};
+
+void insertar (struct Contigua **, int); //Funcion de insertar un elemento a la lista, (Lista, 'El elemento ques se va a agrear')
+
+void eliminar (struct Contigua **, int);//Funcion de eliminar un elemento a la lista, (Lista, 'El elemento ques se va a eliminar'); 'BUSCAR'
+
+void mostrar (struct Contigua *); //Mostrar la lista (Lista)
+
+#endif
