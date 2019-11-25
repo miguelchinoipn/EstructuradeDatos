e1370b17c95c5d78e13c75181134c97d21f9aa96
miguelchinoipn <miguelchinomont@DESKTOP-6GEC3TO.localdomain>
2019-11-24 20:08:00 -0600

    Pila Final

--- Pila/main.c
+++ Pila/main.c
@@ -11,15 +11,11 @@ main (void)
   stack = Push(stack,4);
   stack = Push(stack,5);
 
-  stack = PrintStack(stack);
-
-  //printf("POP\n");    
+  stack = PrintStack(stack);  
   
   stack = Pop(stack);
-  
   stack = Pop(stack);
   
-  printf("most\n");
   stack = PrintStack(stack);
 
   return 0;
