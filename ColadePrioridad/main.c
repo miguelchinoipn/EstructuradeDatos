06388f25b1d5f6d758ae992bd181db753fef7e90
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-26 20:22:28 -0600

    Final

--- ColadePrioridad/main.c
+++ ColadePrioridad/main.c
@@ -1,18 +1,24 @@
 #include"cola.h"
- int
+
+ int
 main ()
 {
-  puts ("Cola Original:");
-  ColaP p = formarP (formarP (formarP (nuevaP (), 3), 2), 1);
-  p = ImpColaP (p);
-  puts ("Agregando 4 a la Cola:");
-  p = formarP (p, 4);
-  p = ImpColaP (p);
-  puts ("Agregando 2 a la Cola:");
-  p = formarP (p, 2);
-  p = ImpColaP (p);
-  puts ("Agregando 0 a la Cola:");
-  p = formarP (p, 0);
-  p = ImpColaP (p);
-  return 0;
-}
+puts ("Cola Original:");
+ColaP p = formarP (formarP (formarP (nuevaP (), 3), 2), 1);
+p = ImpColaP (p);
+  
+  
+puts ("Agregando 4 a la Cola:");
+p = formarP (p, 4);
+p = ImpColaP (p);
+  
+puts ("Agregando 2 a la Cola:");
+p = formarP (p, 2);
+p = ImpColaP (p);
+  
+puts ("Agregando 0 a la Cola:");
+p = formarP (p, 0);
+p = ImpColaP (p);
+  
+return 0;
+}
