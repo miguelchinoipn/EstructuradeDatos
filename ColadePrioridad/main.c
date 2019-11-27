f1b3a3626611ce676175f9ab300bc9c476e909f0
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-26 19:56:52 -0600

    Prioridad

--- ColadePrioridad/main.c
+++ ColadePrioridad/main.c
@@ -3,16 +3,16 @@
 int main(){
   puts("Cola Original:");
   struct ColaPrioridad *p=formarP(formarP(formarP(nuevaP(),3),2),1);
-  p=ImpColaP(p);
+  p=Impstruct(p);
   puts("Agregando 4 a la Cola:");
   p=formarP(p,4);
-  p=ImpColaP(p);
+  p=Impstruct(p);
   puts("Agregando 2 a la Cola:");
   p=formarP(p,2);
-  p=ImpColaP(p);
+  p=Impstruct(p);
   puts("Agregando 0 a la Cola:");
   p=formarP(p,0);
-  p=ImpColaP(p);
+  p=Impstruct(p);
   system("pause");
   return 0;
 }
