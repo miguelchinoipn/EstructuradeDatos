0c19346956e86878711b0a25948aca037699450d
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-28 17:43:19 -0600

    Cola Circular Final

--- ColaCircular/Circular.h
+++ ColaCircular/Circular.h
@@ -3,20 +3,20 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-  struct CCircular
+  struct CCircular
 {
-  int dato;
-   struct CCircular *sig;
- };
-  void ImpElem (int e);
-int EsMoI (int e1, int e2);
-int esIgual (int e1, int e2);
-void carrusel (struct CCircular *c);
-int esnuevaC (struct CCircular *c);
-struct CCircular *formarC (struct CCircular *c, int e);
-int primeroC (struct CCircular *c);
-struct CCircular *desformarC (struct CCircular *c);
-struct CCircular *rotaC (struct CCircular *c);
-struct CCircular *nuevaC ();
- 
+  int dato;
+   struct CCircular *sig;
+ };
+void ImpElem (int e);
+int EsMoI (int e1, int e2);
+int esIgual (int e1, int e2);
+void carrusel (struct CCircular *c);
+int esnuevaC (struct CCircular *c);
+struct CCircular *formarC (struct CCircular *c, int e);
+int primeroC (struct CCircular *c);
+struct CCircular *desformarC (struct CCircular *c);
+struct CCircular *rotaC (struct CCircular *c);
+struct CCircular *nuevaC ();
+ 
 #endif
