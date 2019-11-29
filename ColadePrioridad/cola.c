87a9731de8d8a222c808f959222d60b8c73b8b70
mmonteros-IPN <48659702+miguelchinoipn@users.noreply.github.com>
2019-11-29 10:21:44 -0600

    Final

--- ColadePrioridad/cola.c
+++ ColadePrioridad/cola.c
@@ -1,138 +1,138 @@
 #include"cola.h"
- struct ColaP *
+ struct ColaP *
 nuevaP () 
 {
-   struct ColaP *p = (struct ColaP *) malloc (sizeof (struct ColaP));
-   p->sal = NULL;
-  p->ent = NULL;
-   return p;
-}
+   struct ColaP *p = (struct ColaP *) malloc (sizeof (struct ColaP));
+   p->sal = NULL;
+  p->ent = NULL;
+   return p;
+}
 
-  int 
+  int 
 esnuevaP (struct ColaP *p) 
 {
-   return ((p->ent == NULL) && (p->sal == NULL));
-}
+   return ((p->ent == NULL) && (p->sal == NULL));
+}
 
-  struct ColaP *
+  struct ColaP *
 formarP (struct ColaP *p, int e) 
 {
-   struct Nodo *t = (struct Nodo *) malloc (sizeof (struct Nodo));
-   t->dato = e;
-  t->sig = NULL;
-  t->prioridad = establecarPrioridad (e);
-   if (esnuevaP (p))
+   struct Nodo *t = (struct Nodo *) malloc (sizeof (struct Nodo));
+   t->dato = e;
+   t->sig = NULL;
+   t->prioridad = establecarPrioridad (e);
+   if (esnuevaP (p))
     
     {
-       p->sal = p->ent = t;
-     }
+       p->sal = p->ent = t;
+     }
   
   else
     
     {
-       if (t->prioridad >= p->sal->prioridad)
+       if (t->prioridad >= p->sal->prioridad)
 	
 	{			//Caso nuevo nodo MAYOR a nodo saliente
 	  t->sig = p->sal;
-	   p->sal = t;
-	 }
+	  p->sal = t;
+	}
       
       else if (t->prioridad < p->ent->prioridad)
 	
 	{			//Caso nuevo nodo MENOR a nodo entrante
 	  p->ent->sig = t;
-	   p->ent = t;
-	 }
+	  p->ent = t;
+	 }
       
       else
 	
 	{			//Caso nuevo nodo esta en medio de la cola
 	  struct Nodo *aux = p->sal;
-	   int i = 1;
-	   while (i)
+	   int i = 1;
+	   while (i)
 	    
 	    {
-	       if ((t->prioridad < aux->prioridad) 
+	       if ((t->prioridad < aux->prioridad) 
 		     &&(t->prioridad >= aux->sig->prioridad))
 		
 		{
-		   t->sig = aux->sig;
-		   aux->sig = t;
-		   i = 0;
-		 }
+		   t->sig = aux->sig;
+		   aux->sig = t;
+		   i = 0;
+		 }
 	      
 	      else
 		
 		{
-		   aux = aux->sig;
-		 }
-	     }
-	 }
-     }
-   return p;
-}
+		   aux = aux->sig;
+		 }
+	     }
+	 }
+     }
+   return p;
+}
 
-  int 
+  int 
 primeroP (struct ColaP *p) 
 {
-   return p->sal->dato;
-}
+   return p->sal->dato;
+}
 
-  struct ColaP *
+  struct ColaP *
 desformarP (struct ColaP *p) 
 {
-   struct Nodo *t = p->sal;
-   if (p->sal == p->ent)
-     p = nuevaP ();
-   
+   struct Nodo *t = p->sal;
+   if (p->sal == p->ent)
+     p = nuevaP ();
+   
   else
-     p->sal = p->sal->sig;
-   free (t);
-   return p;
-}
+     p->sal = p->sal->sig;
+  free (t);
+   return p;
+}
 
-  struct ColaP *
+  struct ColaP *
 ImpColaP (struct ColaP *p) 
 {
-   struct ColaP *c = nuevaP ();
-   while (!esnuevaP (p))
+   struct ColaP *c = nuevaP ();
+   while (!esnuevaP (p))
     
     {
-       c = formarP (c, primeroP (p));
-       Impint (primeroP (p));
-       p = desformarP (p);
-     }
-   free (p);
-   return c;
-}
+       c = formarP (c, primeroP (p));
+       Impint (primeroP (p));
+       p = desformarP (p);
+    }
+   free (p);
+   return c;
+}
 
-  void 
+  void 
 Impint (int e) 
 {
-   printf ("%d\n", e);
-}  int 
+   printf ("%d\n", e);
+}  int 
 
 esMoI (int e1, int e2) 
 {
-   return e1 >= e2;
-}
+   return e1 >= e2;
+}
 
-  int 
+  int 
 esMenor (int e1, int e2) 
 {
-   return e1 < e2;
-}
+   return e1 < e2;
+}
 
-  int 
+  int 
 esIgual (int e1, int e2) 
 {
-   return e1 == e2;
-}
+   return e1 == e2;
+}
 
-  int 
+  int 
 establecarPrioridad (int e) 
 {
-   return e;
-}
+   return e;
+}
 
 
--- ColadePrioridad/main.c
+++ ColadePrioridad/main.c
@@ -1,20 +1,20 @@
 #include"cola.h"
- int 
+ int 
 main () 
 {
-  puts ("Cola Original:");
-  struct ColaP *p = formarP (formarP (formarP (nuevaP (), 3), 2), 1);
-  p = ImpColaP (p);
-    puts ("\nAgregando 4 a la Cola:");
-  p = formarP (p, 4);
-  p = ImpColaP (p);
-   puts ("\nAgregando 2 a la Cola:");
-  p = formarP (p, 2);
-  p = ImpColaP (p);
-   puts ("\nAgregando 0 a la Cola:");
-  p = formarP (p, 0);
-  p = ImpColaP (p);
-   return 0;
-}
+  puts ("Cola Original:");
+  struct ColaP *p = formarP (formarP (formarP (nuevaP (), 3), 2), 1);
+  p = ImpColaP (p);
+  puts ("\nAgregando 4 a la Cola:");
+  p = formarP (p, 4);
+  p = ImpColaP (p);
+  puts ("\nAgregando 2 a la Cola:");
+  p = formarP (p, 2);
+  p = ImpColaP (p);
+  puts ("\nAgregando 0 a la Cola:");
+  p = formarP (p, 0);
+  p = ImpColaP (p);
+  return 0;
+}
 
 
